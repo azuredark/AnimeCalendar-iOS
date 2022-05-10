c762161d06be56c4819b77a3f660b8675cd59ab3
Leonardo <lmestremadoyro@gmail.com>
2022-05-09 19:08:12 -0500

    deleted unnecessary classes

--- AnimeCalendar/AnimeCalendar/Scenes/TabBar/TabBarManager.swift
+++ AnimeCalendar/AnimeCalendar/Scenes/TabBar/TabBarManager.swift
@@ -12,21 +12,10 @@ final class TabBarManager: RootViewController {
   private let requestsManager: RequestProtocol
   private var tabBarController: TabBarWithMiddleButton = CustomTabBarController()
 
-  private var tabBarItems = [TabBarItem]()
-
   init(_ requestsManager: RequestProtocol) {
     self.requestsManager = requestsManager
     configureTabManager()
   }
-
-//  fileprivate func configureTabScreens() {
-//    let tabFactory = TabBarFactory(requestsManager)
-//
-//    let homeTabItem: TabBarItem = tabFactory.getTabBarScreen(.homeTab)
-//    let newScheduledAnimeTabItem: TabBarItem = tabFactory.getTabBarScreen(.newAnimeTab)
-//    let animeCalendarTabItem: TabBarItem = tabFactory.getTabBarScreen(.animeCalendarTab)
-//    tabBarItems = [homeTabItem, newScheduledAnimeTabItem, animeCalendarTabItem]
-//  }
 
   fileprivate func configureTabController() {
     let screenFactory = ScreenFactory(requestsManager)
@@ -36,7 +25,6 @@ final class TabBarManager: RootViewController {
     let animeCalendarScreen = screenFactory.getRootScreen(.animeCalendarScreen).getRootViewController()
 
     tabBarController.viewControllers = [homeScreen, newAnimeScreen, animeCalendarScreen]
-//    tabBarController.viewControllers = [tabBarItems[0].wrapNavigation(), tabBarItems[2].wrapNavigation()]
   }
 
   fileprivate func configureTabBarMiddleButton() {
@@ -53,16 +41,6 @@ final class TabBarManager: RootViewController {
 //      print("PRESENT VC")
 //    }
   }
-
-//  fileprivate func configureTabItems() {
-//    guard let tabBarControllerItems = tabBarController.tabBar.items else { return }
-//    for (tabBarControllerItem, tabBarItem) in zip(tabBarControllerItems, tabBarItems) {
-//      tabBarControllerItem.badgeValue = tabBarItem.tabBadge
-//      tabBarControllerItem.title = tabBarItem.tabTitle
-//      tabBarControllerItem.image = tabBarItem.tabImage
-//      tabBarControllerItem.isEnabled = tabBarItem.enabled
-//    }
-//  }
 
   fileprivate func configureTabManager() {
 //    configureTabScreens()
